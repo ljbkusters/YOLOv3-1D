5d8df61ca926131ea6644c869f4a720fd369f70e
Luc Kusters <luc.kusters@anatech.nl>
2023-11-14 14:34:00 +0100

    generate_curves: fix curve generation
    Fix some major issues with curve generation.

--- generate_curves.py
+++ generate_curves.py
@@ -25,14 +25,15 @@ def gaussian(x, A, mu, sig, nsigma=4, randomness=True):
 def skew_gaussian(x, A, mu, sig, skewness, nsigma=4, randomness=True):
     onset = max(mu - nsigma * sig, min(x))
     end = min(mu + nsigma * sig, max(x))
-    gaussian_curve, o, e = gaussian(x, 1, mu, sig, randomness=False)
-    curve = (A / 2 * (1 + numpy.tanh((x-mu)*skewness)) * gaussian_curve
+    gaussian_curve, o, e = gaussian(x, A, mu, sig, randomness=False)
+    sigmoid = 0.5 * (1 + numpy.tanh((x-mu)*skewness))
+    curve = (abs(sigmoid * gaussian_curve)
              + randomness * numpy.random.normal(0, RANDOM_SIGMA, len(x)))
     return (curve, onset, end)
 
 def lorentzian(x, A, mu, gamma, randomness=True):
-    onset = max(mu - 2.5 * gamma, min(x))
-    end = min(mu + 2.5 * gamma, max(x))
+    onset = max(mu - 5 * gamma, min(x))
+    end = min(mu + 5 * gamma, max(x))
     curve = (A * (gamma**2 / (gamma**2 + (x-mu)**2))
              + randomness * numpy.random.normal(0, RANDOM_SIGMA, len(x)))
     return (curve, onset, end)
@@ -50,22 +51,41 @@ if __name__ == "__main__":
     x = numpy.linspace(XMIN, XMAX, NX)
     print("generating curves...")
 
-    N_curves = 10000
+    N_curves = 20_000   # curves per type
     random_sigmas = numpy.random.uniform(1, 4, N_curves)
-    random_mus = numpy.random.uniform(20, 40, N_curves)
+    random_mus = numpy.random.uniform(10, 40, N_curves)
     random_amplitudes = numpy.random.uniform(1, 3, N_curves)
 
     curves = []
+    # single peak gaussians
     for A, mu, sig in zip(random_amplitudes, random_mus, random_sigmas):
         gaussian_, onset, end = gaussian(x, A, mu, sig,
                                          randomness=False)
         target_label = (onset, end, "gaussian")
         curves.append((gaussian_, [target_label]))
 
-    random_sigmas = numpy.random.uniform(1, 4, N_curves)
-    random_mus = numpy.random.uniform(20, 40, N_curves)
-    random_amplitudes = numpy.random.uniform(1, 3, N_curves)
-    random_skewness = numpy.random.uniform(-2, -1, N_curves)
+    # double peaks
+    random_sigmas_1 = numpy.random.uniform(1, 2, N_curves)
+    random_sigmas_2 = numpy.random.uniform(1, 2, N_curves)
+    random_mus_1 = numpy.random.uniform(10, 20, N_curves)
+    random_mus_2 = numpy.random.uniform(30, 40, N_curves)
+    random_amplitudes_1 = numpy.random.uniform(1, 3, N_curves)
+    random_amplitudes_2 = numpy.random.uniform(1, 3, N_curves)
+
+    for A_1, mu_1, sig_1, A_2, mu_2, sig_2 in zip(random_amplitudes_1, random_mus_1, random_sigmas_1,
+                                random_amplitudes_2, random_mus_2, random_sigmas_2):
+        gaussian_left, onset_left, end_left = gaussian(x, A_1, mu_1, sig_1,
+                                         randomness=False)
+        gaussian_right, onset_right, end_right = gaussian(x, A_2, mu_2, sig_2,
+                                         randomness=False)
+        target_label_left = (onset_left, end_left, "gaussian")
+        target_label_right = (onset_right, end_right, "gaussian")
+        curves.append((gaussian_left + gaussian_right, [target_label_left, target_label_right]))
+
+    random_sigmas = numpy.random.uniform(1, 4, N_curves//2)
+    random_mus = numpy.random.uniform(10, 40, N_curves//2)
+    random_amplitudes = numpy.random.uniform(1, 3, N_curves//2)
+    random_skewness = numpy.random.uniform(-2, -1, N_curves//2)
 
     for A, mu, sig, skew in zip(random_skewness, random_mus, random_sigmas, random_skewness):
         skew_gaussian_, onset, end = skew_gaussian(x, A, mu, sig, skew,
@@ -73,10 +93,10 @@ if __name__ == "__main__":
         target_label = (onset, end, "skew_gaussian")
         curves.append((skew_gaussian_, [target_label]))
 
-    random_sigmas = numpy.random.uniform(1, 4, N_curves)
-    random_mus = numpy.random.uniform(20, 40, N_curves)
-    random_amplitudes = numpy.random.uniform(1, 3, N_curves)
-    random_skewness = numpy.random.uniform(1, 2, N_curves)
+    random_sigmas = numpy.random.uniform(1, 4, N_curves//2)
+    random_mus = numpy.random.uniform(10, 40, N_curves//2)
+    random_amplitudes = numpy.random.uniform(1, 3, N_curves//2)
+    random_skewness = numpy.random.uniform(1, 2, N_curves//2)
 
     for A, mu, sig, skew in zip(random_amplitudes, random_mus, random_sigmas, random_skewness):
         skew_gaussian_, onset, end = skew_gaussian(x, A, mu, sig, skew,
@@ -85,7 +105,7 @@ if __name__ == "__main__":
         curves.append((skew_gaussian_, [target_label]))
 
     random_gammas = numpy.random.uniform(1, 2, N_curves)
-    random_mus = numpy.random.uniform(20, 40, N_curves)
+    random_mus = numpy.random.uniform(10, 40, N_curves)
     random_amplitudes = numpy.random.uniform(1, 3, N_curves)
 
     for A, mu, gamma in zip(random_amplitudes, random_mus, random_gammas):
@@ -109,8 +129,8 @@ if __name__ == "__main__":
     # YOLOv3-1D format saving
     import os
     #os.makedirs("data", exist_ok=True)
-    os.makedirs(os.path.join("data", "1d_series"), exist_ok=True)
-    os.makedirs(os.path.join("data", "labels"), exist_ok=True)
+    os.makedirs(os.path.join("data", "test_data", "1d_series"), exist_ok=True)
+    os.makedirs(os.path.join("data", "test_data", "labels"), exist_ok=True)
     curves_series = numpy.array([c[0] for c in curves])
     if True:
         print("normalizing curves...")
@@ -152,14 +172,16 @@ if __name__ == "__main__":
     annotations = [(f"series_{i}.npy",
                     f"labels_{i}.csv")
                    for i, _ in enumerate(curves_labels)]
-    numpy.savetxt(os.path.join("data", "test_data", "annotations.csv"), annotations,
-                  delimiter=", ", fmt="%s")
 
     PERCENTAGE_TRAIN = 0.95
     N_CUTOFF = int(len(annotations)*0.95)
 
-    shuffeled_annotations = numpy.random.shuffle(annotations)
+    permutation = numpy.random.permutation(range(len(annotations)))
+    shuffeled_annotations = [annotations[p] for p in permutation]
 
+    numpy.savetxt(os.path.join("data", "test_data", "annotations.csv"),
+                  annotations,
+                  delimiter=", ", fmt="%s")
     numpy.savetxt(os.path.join("data", "test_data", "train_annotations.csv"),
                   shuffeled_annotations[:N_CUTOFF],
                   delimiter=", ", fmt="%s")
